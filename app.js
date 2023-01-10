a02592285c9b41162b6d08fc4e68eaf10712e2e5
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-01-10 15:43:19 +0100

    fix: validation

--- app.js
+++ app.js
@@ -5,52 +5,102 @@ const color = document.querySelector("#color");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const errorElement = document.getElementById("error");
-const submitButton = document.querySelector("submit_btn");
+const submitButton = document.querySelector("#submit_btn");
 
-//validation of the First Name
+let format = /[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+// first name validation
+
 firstName.addEventListener("input", function (e) {
-  if (
-    firstName.value[0] &&
-    firstName.value[0] === firstName.value[0].toUpperCase()
-  ) {
-    firstName.classList.remove("is-danger");
-    firstName.classList.add("is-success");
+  if (firstName.value && format.test(firstName.value)) {
+    errorElement.innerText = "Your name could not contain special characters";
+    firstName.classList.remove("is-success");
+    firstName.classList.add("is-danger");
+    return;
   }
   if (
     firstName.value[0] &&
     firstName.value[0] !== firstName.value[0].toUpperCase()
   ) {
+    errorElement.innerText =
+      "The first name should start with a capital letter";
+    firstName.classList.remove("is-success");
+    firstName.classList.add("is-danger");
+    return;
+  }
+  errorElement.innerText = "";
+  firstName.classList.remove("is-danger");
+
+  if (firstName.value.length < 3 && firstName.value.length > 0) {
+    errorElement.innerText = "Your name must be at least 3 characters long";
     firstName.classList.remove("is-success");
     firstName.classList.add("is-danger");
   }
+  if (firstName.value.length >= 3) {
+    errorElement.innerText = "";
+    firstName.classList.remove("is-danger");
+    firstName.classList.add("is-success");
+  }
 });
 
-//validation of the Last Name
+// last name validation
+
 lastName.addEventListener("input", function (e) {
-  if (
-    lastName.value[0] &&
-    lastName.value[0] === lastName.value[0].toUpperCase()
-  ) {
-    lastName.classList.remove("is-danger");
-    lastName.classList.add("is-success");
+  if (lastName.value && format.test(lastName.value)) {
+    errorElement.innerText = "Your name could not contain special characters";
+    lastName.classList.remove("is-success");
+    lastName.classList.add("is-danger");
+    return;
   }
   if (
     lastName.value[0] &&
     lastName.value[0] !== lastName.value[0].toUpperCase()
   ) {
+    errorElement.innerText = "The last name should start with a capital letter";
+    lastName.classList.remove("is-success");
+    lastName.classList.add("is-danger");
+    return;
+  }
+  errorElement.innerText = "";
+  lastName.classList.remove("is-danger");
+
+  if (lastName.value.length < 3 && lastName.value.length > 0) {
+    errorElement.innerText = "Your name must be at least 3 characters long";
     lastName.classList.remove("is-success");
     lastName.classList.add("is-danger");
   }
+  if (lastName.value.length >= 3) {
+    errorElement.innerText = "";
+    lastName.classList.remove("is-danger");
+    lastName.classList.add("is-success");
+  }
 });
 
-//validation of the Email -
+//password validation
 
-// here should not be input, i've tried change - no succes, i am keep thinking
+password.addEventListener("input", function (e) {
+  if (password.value.length <= 6) {
+    password.classList.remove("is-success");
+    password.classList.add("is-danger");
+    errorElement.innerText = "Password should be longer than 6 characters";
+  } else if (password.value.length > 6 && password.value.length < 20) {
+    password.classList.remove("is-danger");
+    password.classList.add("is-success");
+    errorElement.innerText = "";
+  } else if (password.value.length >= 20) {
+    password.classList.remove("is-success");
+    password.classList.add("is-danger");
+    errorElement.innerText = "Password should be shorter than 20 characters";
+  }
+});
+
+//changing button, if use fulfilled the form (add in condition color and email)
+
 form.addEventListener("input", function (e) {
   if (
-    firstName.classList.contains("is_succes") &&
-    lastName.classList.contains("is_succes") &&
-    email.classList.contains("is_succes")
+    firstName.classList.contains("is-success") &&
+    lastName.classList.contains("is-success") &&
+    password.classList.contains("is-success")
   ) {
     submitButton.removeAttribute("disabled");
   }
@@ -59,30 +109,6 @@ form.addEventListener("input", function (e) {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   alert(
-    `My name is ${firstName.value} ${lastName.value},\nthe email is ${myEmail.value}.\nAnd my favorite color is ${myColor.value}`
+    `My name is ${firstName.value} ${lastName.value},\nthe email is ${email.value}.\nAnd my favorite color is ${color.value}`
   );
 });
-
-// form.addEventListener("submit", (e) => {
-//   let messages = [];
-//   if (name.value === "" || name.value == null) {
-//     messages.push("Name is required");
-//   }
-
-//   if (password.value.length <= 6) {
-//     messages.push("Password must be longer than 6 characters");
-//   }
-
-//   if (password.value.length >= 20) {
-//     messages.push("Password must be less than 20 characters");
-//   }
-
-//   if (password.value === "password") {
-//     messages.push("Password cannot be password");
-//   }
-
-//   if (messages.length > 0) {
-//     e.preventDefault();
-//     errorElement.innerText = messages.join(", ");
-//   }
-// });
